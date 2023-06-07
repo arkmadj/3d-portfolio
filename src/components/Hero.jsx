44120c7fb2b6721ca79e5d138b150206decadb67
arkmadj <ahmadjinadu@gmail.com>
2023-06-07 10:12:54 +0100

    added scroll icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,11 +30,17 @@ const Hero = () => {
 			<ComputersCanvas />
 			<div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
 				<a href="#about">
-					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
-            <motion.div>
-              
-            </motion.div>
-          </div>
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
+						/>
+					</div>
 				</a>
 			</div>
 		</section>
