f54508b65c252e6a5f4bae214140cf5da3294e75
arkmadj <ahmadjinadu@gmail.com>
2023-06-07 09:32:07 +0100

    added computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { useGLTF, OrbitControls, Preload } from "@react-three/drei"
+
+import CanvasLoader from "./Loader"
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <div>Computers</div>
   )
