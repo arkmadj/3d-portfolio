39a0846e67d6397c6929ce9b9eae09712cc8a766
arkmadj <ahmadjinadu@gmail.com>
2023-06-08 22:36:45 +0100

    added about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,34 @@
-import Tilt  from "react-tilt"
-import { motion } from "framer-motion"
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-
-import {styles} from "../styles"
-import {services} from "../contants"
-import {fadeIn, textVariant} from "../utils/motion"
+import { styles } from "../styles";
+import { services } from "../contants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <>
-      <motion.div>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div>
-    </>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview</h2>
+			</motion.div>
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+			>
+				As a software engineer with over 5 years of experience, I have a proven
+				track record of designing, developing, and deploying innovative
+				solutions. I specialize in full-stack development, with a particular
+				focus on Javascript and its associated frameworks. I have experience
+				working on a wide range of projects, from small web apps to large-scale
+				enterprise systems. My skills include proficiency in programming
+				languages such as Python, JavaScript, Java, Go, and C++, as well as
+				experience with HTML, CSS, and various frameworks and tools such as
+				ReactJS, NodeJS, ExpressJS, Flask, Django, AWS, Azure, GCP, Git, Agile,
+				Scrum.
+			</motion.p>
+		</>
+	);
+};
 
-export default About
+export default About;
