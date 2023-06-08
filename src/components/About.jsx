712d289483751d19c083b9c388cb34fe7a659a52
arkmadj <ahmadjinadu@gmail.com>
2023-06-08 22:52:11 +0100

    added tilt card contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,17 @@ const ServiceCard = ({ index, title, icon }) => {
 			<motion.div
 				variants={fadeIn("right", "spring", 0.5 * index, 0.5)}
 				className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-			></motion.div>
+			>
+				<div
+					options={{ max: 45, scale: 1, speed: 450 }}
+					className="bg-tertiary rounded-[29px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+				>
+					<img src={icon} alt={title} className="object-contain w-16 h-16" />
+					<h3 className="text-white text-[20px] font-bold text-center">
+						{title}
+					</h3>
+				</div>
+			</motion.div>
 		</Tilt>
 	);
 };
