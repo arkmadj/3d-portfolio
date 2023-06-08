c4c002ff45d878582363264468f2b9b23bcf59f3
arkmadj <ahmadjinadu@gmail.com>
2023-06-08 22:29:22 +0100

    added imports

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,11 @@
 import Tilt  from "react-tilt"
 import { motion } from "framer-motion"
 
+
+import {styles} from "../styles"
+import {services} from "../contants"
+import {fadeIn, textVariant} from "../utils/motion"
+
 const About = () => {
   return (
     <div>About</div>
