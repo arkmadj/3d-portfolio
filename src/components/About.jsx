084600108fbab0819615347b55e6221c317686af
arkmadj <ahmadjinadu@gmail.com>
2023-06-08 22:53:45 +0100

    fixed border radius

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const ServiceCard = ({ index, title, icon }) => {
 			>
 				<div
 					options={{ max: 45, scale: 1, speed: 450 }}
-					className="bg-tertiary rounded-[29px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+					className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
 				>
 					<img src={icon} alt={title} className="object-contain w-16 h-16" />
 					<h3 className="text-white text-[20px] font-bold text-center">
