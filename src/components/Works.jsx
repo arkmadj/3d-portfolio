72f918ce5669d6031eee006f583c306cde9363c2
arkmadj <ahmadjinadu@gmail.com>
2023-06-09 23:05:25 +0100

    added header

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../contants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>What I have done so far</p>
+				<h2 className={styles.sectionHeadText}>Work Exprience</h2>
+			</motion.div>
+		</>
+	);
+};
 
-export default Works
+export default SectionWrapper(Works, "");
