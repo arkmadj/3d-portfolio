929adc30f925d4616ef3969dfd4751aaae64ed33
arkmadj <ahmadjinadu@gmail.com>
2023-06-10 07:49:26 +0100

    added works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,13 +17,21 @@ const ProjectCard = ({
 }) => {
 	return (
 		<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-			<Tilt options={{ max: 45, scale: 1, speed: 450 }}>
+			<Tilt
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+			>
 				<div className="relative w-full h-[230px]">
 					<img
 						src={image}
-						alt={name}
+						alt="project_image"
 						className="object-cover w-full h-full rounded-2xl"
 					/>
+
 					<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
 						<div
 							onClick={() => window.open(source_code_link, "_blank")}
@@ -31,15 +39,27 @@ const ProjectCard = ({
 						>
 							<img
 								src={github}
-								alt="github"
-								className="w=1/2 h-1/2 object-contain"
+								alt="source code"
+								className="object-contain w-1/2 h-1/2"
 							/>
 						</div>
 					</div>
 				</div>
+
 				<div className="mt-5">
-					<h3>{name}</h3>
-					<p>{description}</p>
+					<h3 className="text-white font-bold text-[24px]">{name}</h3>
+					<p className="mt-2 text-secondary text-[14px]">{description}</p>
+				</div>
+
+				<div className="flex flex-wrap gap-2 mt-4">
+					{tags.map((tag) => (
+						<p
+							key={`${name}-${tag.name}`}
+							className={`text-[14px] ${tag.color}`}
+						>
+							#{tag.name}
+						</p>
+					))}
 				</div>
 			</Tilt>
 		</motion.div>
@@ -50,9 +70,10 @@ const Works = () => {
 	return (
 		<>
 			<motion.div variants={textVariant()}>
-				<p className={styles.sectionSubText}>My work</p>
-				<h2 className={styles.sectionHeadText}>Projects.</h2>
+				<p className={`${styles.sectionSubText} `}>My work</p>
+				<h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
 			</motion.div>
+
 			<div className="flex w-full">
 				<motion.p
 					variants={fadeIn("", "", 0.1, 1)}
@@ -65,9 +86,10 @@ const Works = () => {
 					and manage projects effectively.
 				</motion.p>
 			</div>
+
 			<div className="flex flex-wrap mt-20 gap-7">
 				{projects.map((project, index) => (
-					<ProjectCard key={`project-${index}`} {...project} index={index} />
+					<ProjectCard key={`project-${index}`} index={index} {...project} />
 				))}
 			</div>
 		</>
