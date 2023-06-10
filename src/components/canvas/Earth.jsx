ec3c1769c3b91c62ea9f308528aa3efd91e3c38a
arkmadj <ahmadjinadu@gmail.com>
2023-06-10 09:26:43 +0100

    added earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,9 @@ import CanvasLoader from "../Loader";
 
 const Earth = () => {
 	const earth = useGLTF("./planet/scene.gltf");
-	return <div>Earth</div>;
+	return (
+		<primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+	);
 };
 
 const EarthCanvas = () => {
