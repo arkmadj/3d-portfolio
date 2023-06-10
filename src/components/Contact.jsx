95b9a1eedf3057ddb1494f5fd2e4167221d0e2e8
arkmadj <ahmadjinadu@gmail.com>
2023-06-10 08:50:20 +0100

    added form state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		messgae: "",
+	});
+  
+	return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
